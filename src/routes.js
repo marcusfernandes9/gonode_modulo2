9325b69a932424dd28f12e0983260b67bbe97969
Marcus Fernandes <marcus.fernandes9@gmail.com>
2019-03-21 23:33:04 -0300

    Criptografia de senha

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const routes = express.Router()
+const UserController = require('./app/controllers/UserController')
 
-routes.get('/', (req, res) => {
-  return res.send('Hello Word')
-})
+routes.get('/signup', UserController.create)
+routes.post('/signup', UserController.store)
 
 module.exports = routes
