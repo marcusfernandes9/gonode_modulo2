3185cbd319849dcf5c18f240078e61fd5329a810
Marcus Souza Fernandes <marcus.fernandes9@gmail.com>
2019-05-19 16:34:49 -0300

    Formulário de Agendamento com Horários disponíveis e Agendamento

--- src/app/models/Appointment.js
+++ src/app/models/Appointment.js
@@ -1,6 +1,6 @@
 module.exports = (sequelize, DataTypes) => {
   const Appointment = sequelize.define('Appointment', {
-    date: DataTypes.date
+    date: DataTypes.DATE
   })
 
   Appointment.associate = models => {
